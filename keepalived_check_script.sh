e4d1dc97c2fc6761158f79b29f9425df6369a3d9
Daniel Sol <3523948+szolll@users.noreply.github.com>
2020-06-10 11:18:19 +0200

    First
    me

---
+++ keepalived_check_script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Keepalived check script / Check if services are running on the localhost
+#
+# Usage: keepcheck.sh [service's! you want to have checked]
+# Example: keepcheck.sh sshd nginx mariadb your_custom_script
+# Place the script somewhere ( /usr/local/bin/ )
+# Prints 0 for "Ok", other than 1 for "service is broken"
+
+CHECK=$0
+SERVICE=$1
+
+# For each service provided check if it's running
+for word in "$@";
+do
+   # Get state
+   OUTPUT=$(ps aux | grep -v grep | grep -v $CHECK |grep $word)
+   # If its running $STATE = 1
+   if [ "${#OUTPUT}" -gt 0 ] ; then 
+      STATE=0
+      # If it's not running print 0 and exit
+	  else 
+	     echo '1'
+	  exit 1
+   fi
+done
+
+# Lastly check if the state is 0/OK! if so print 0
+if [ "$STATE" = '0' ]; then
+   echo '0'
+fi
